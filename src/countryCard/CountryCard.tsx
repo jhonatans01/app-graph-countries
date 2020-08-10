1bd9ec3917faf3732ba3e4582b6345c9e54ae891
Jhonatan Silva <jhonatansilva96@gmail.com>
2020-08-10 19:52:41 -0300

    initializes repository and adds react-router

---
+++ src/countryCard/CountryCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CountryCard() {
+  return <div>test country card</div>;
+}
+
+export default CountryCard;
---
+++ src/countryCard/CountryCards.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CountryCards() {
+  return <div>test country card</div>;
+}
+
+export default CountryCards;
