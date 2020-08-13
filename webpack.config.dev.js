c22a8344ccde2d87b579e826df9449a3ad5e90d8
Jhonatan Silva <jhonatansilva96@gmail.com>
2020-08-13 01:38:05 -0300

    fixes routing in dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,4 +3,9 @@ const baseConfig = require("./webpack.config.base.js");
 
 module.exports = merge(baseConfig, {
   mode: "development",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: "./",
+    hot: true,
+  },
 });
